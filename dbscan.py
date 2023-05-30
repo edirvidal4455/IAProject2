f6e4befb1cb63011800292adc65ebff289b9aba3
ibanesss121232 <105462844+ibanesss121232@users.noreply.github.com>
2023-05-29 22:26:52 -0500

    Update dbscan.py

--- dbscan.py
+++ dbscan.py
@@ -1,59 +1,63 @@
-class DBSCAN:
-    def __init__(self, eps, min_samples):
-        self.eps = eps
-        self.min_samples = min_samples
+class DBScan:
+
+    def __init__(self, epsilon, puntos_minimos): #parametro de entrada
+        self.puntos_minimos =puntos_minimos
+        self.eps = epsilon
         self.labels = None
         self.visited = set()
 
-    def dbscan(self, dataset):
-        self.labels = [0] * len(dataset)
-        cluster_label = 0
+    def fit(self, data):
+        self.labels=np.zeros(len(data))   # Un array  de ceros con len de X
+        cluster_label = 0                  #esto nos ira por cada punto 
 
-        for i, point in enumerate(dataset):
-            if i in self.visited:
+        for indice, punto in enumerate(data):
+            if indice in self.visited:
                 continue
-            self.visited.add(i)
-            neighbors = self._region_query(dataset, point)
+            self.visited.add(indice)
+            neighbors = self.vecinos_cercanos(data, punto)
 
-            if len(neighbors) < self.min_samples:
-                self.labels[i] = -1
+            if len(neighbors) < self.puntos_minimos:
+                self.labels[indice] = -1
             else:
-                cluster_label += 1
-                self._expand_cluster(dataset, i, neighbors, cluster_label)
+                cluster_label =cluster_label + 1
+                self.explandir_cluster(data, indice, neighbors, cluster_label)
 
         return self.labels
 
-    def _expand_cluster(self, dataset, point_index, neighbors, cluster_label):
+    def explandir_cluster(self, dataset, point_index, vecinos, cluster_label):
+
         self.labels[point_index] = cluster_label
 
-        while neighbors:
-            current_point = neighbors.pop(0)
+        while vecinos:
+            current_point = vecinos.pop(0)
             if current_point not in self.visited:
                 self.visited.add(current_point)
-                current_neighbors = self._region_query(dataset, dataset[current_point])
+                current_neighbors = self.vecinos_cercanos(dataset, dataset[current_point])
 
-                if len(current_neighbors) >= self.min_samples:
-                    neighbors.extend(current_neighbors)
+                if len(current_neighbors) >= self.puntos_minimos:
+                    vecinos.extend(current_neighbors)
 
             if self.labels[current_point] == 0:
                 self.labels[current_point] = cluster_label
 
-    def _region_query(self, dataset, point):
-        neighbors = []
-        for i, neighbor in enumerate(dataset):
-            if self._euclidean_distance(point, neighbor) <= self.eps:
-                neighbors.append(i)
-        return neighbors
+    def vecinos_cercanos(self, datafrane, punto): # agregamos las los vecinos que estan dentro eps
+        vecinos = []
+        for i, vecino in enumerate(datafrane):
+            if self.distancia(punto, vecino) <= self.eps:
+                vecinos.append(i)
+        return vecinos
 
     @staticmethod
-    def _euclidean_distance(point_a, point_b):
-        squared_distance = sum([(a - b) ** 2 for a, b in zip(point_a, point_b)])
-        return squared_distance ** 0.5
+    def distancia(a, b): #calculamos la distancia con euclidiana
+        dis = (sum([(a - b) ** 2 for a, b in zip(a,b)]))**0.5
+        return dis
+
+
 
 
 # Example usage
 if __name__ == '__main__':
     dataset = [[1, 1], [1, 2], [2, 1], [10, 10], [10, 11], [11, 10], [20, 20]]
     dbscan = DBSCAN(eps=3, min_samples=2)
-    labels = dbscan.dbscan(dataset)
+    labels = dbscan.train(dataset)
     print(labels)
